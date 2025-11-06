48010005aa52e4ff5b84398855fc229333d5250e
AdaTomruk <adatomruk10@gmail.com>
2025-11-06 18:13:12 +0300

    Initial Commit

---
+++ Desmos Menubar/Desmos_MenubarApp.swift
@@ -0,0 +1,17 @@
+//
+//  Desmos_MenubarApp.swift
+//  Desmos Menubar
+//
+//  Created by Ada Tomruk on 6.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct Desmos_MenubarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
